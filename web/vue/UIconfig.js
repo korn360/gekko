9205b43a0856e9f018fecd5a84158d759a48e4b6
korn360 <korn360@gmail.com>
2018-03-18 02:59:25 +0000

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -7,13 +7,13 @@ const CONFIG = {
   headless: false,
   api: {
     host: '127.0.0.1',
-    port: 3000,
+    port: 3001,
     timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
     host: 'localhost',
-    port: 3000,
+    port: 3001,
     path: '/'
   },
   adapter: 'sqlite'
